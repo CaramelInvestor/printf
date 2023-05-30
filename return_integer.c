b3ee5de17611df050d360295dc68ca5d8f357158
CaramelInvestor <nnandibekee@gmail.com>
2023-05-30 20:11:38 +0000

    latest changes

---
+++ return_integer.c
@@ -0,0 +1,46 @@
+#include "main.h"
+/**
+ * output_integer - function that handles d
+ * Description: makes convertion of integer
+ * @arg: arguments
+ * @s: char pointer
+ * Return: returns count of digits
+ */
+int output_integer(const char *s, va_list arg)
+{
+	int a[18], h;
+	long int k = 1, n, m = 100000000000000000;
+
+	int counter = 0, sum = 0;
+
+	h = hight(s, 'i');
+	(h == 2) ? (n = va_arg(arg, long int)) : (n = va_arg(arg, int));
+	if (h == 1)
+		n = (short) n;
+	if (n == 0)
+		counter += my_putchar('0');
+	if (n < 0)
+	{
+		n *= -1;
+		my_putchar('-');
+		counter++;
+	}
+	a[0] = n / m;
+
+	for (; k < 18; k++)
+	{
+		m /= 10;
+		a[k] = (n / m) % 10;
+	}
+
+	for (k = 0; k < 18; k++)
+	{
+		sum += a[k];
+		if (sum != 0 || k == 18)
+		{
+			my_putchar('0' + a[k]);
+			counter++;
+		}
+	}
+	return (counter);
+}
